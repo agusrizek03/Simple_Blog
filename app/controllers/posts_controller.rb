183d3f4499f9407e31b8074519234d3dadb1a25e
Agus Dwi Krismono <dwiagus85@gmail.com>
2018-11-20 16:14:26 +0700

    add and view post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @post = current_user.posts.all
   end
 
   def show
@@ -8,9 +9,24 @@ class PostsController < ApplicationController
   def edit
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    @post.save
+    redirect_to posts_path
+  end
+
   def update
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
