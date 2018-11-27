fbe1294b3bd79b4307c830c7d2ed51c0963515db
Agus Dwi Krismono <dwiagus85@gmail.com>
2018-11-27 07:53:11 +0700

    edit in destroy images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,8 +1,9 @@
 class ImagesController < ApplicationController
 
 	def destroy
-		@image = Image.find(params[:id])
+		@post = Post.find(params[:post_id])
+		@image = @post.images.find(params[:id])
 		@image.destroy
-		redirect_to posts_path
+		redirect_to edit_post_path(@post)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :posts
-  resources :images
-  resources :comments
+  resources :posts do
+  	resources :images
+  	resources :comments
+  end
 end
